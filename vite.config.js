0810b54274e6f392f351ee3c03dc5d57c2c0a264
Bhimgouda D Patil <satvikp508@gmail.com>
2024-11-09 00:57:57 +0530

    added vercel json

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-import process from "node:process";
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    process: process,
-    global: {},
-  },
 });
