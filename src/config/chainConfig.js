ab3a048b088db8f3dda80b226fdf5a648ccf930a
Bhimgouda D Patil <satvikp508@gmail.com>
2024-11-28 02:36:29 +0530

    major changes

--- src/config/chainConfig.js
+++ src/config/chainConfig.js
@@ -1,74 +1,126 @@
-export const chainConfig = {
-  // 89346162: {
-  //   name: "Reya Cronos",
-  //   id: 89346162,
-  //   logo: "/logo/reya-logo.webp",
-  //   api: "https://api.spiralstake.xyz",
-  //   nativeCurrency: {
-  //     name: "Ether",
-  //     symbol: "ETH",
-  //     decimals: 18,
-  //   },
-  //   onboard: {
-  //     amountNative: "0.0001",
-  //   },
-  //   default: {
-  //     http: ["https://rpc.reya-cronos.gelato.digital"],
-  //     webSocket: ["wss://ws.reya-cronos.gelato.digital"],
-  //   },
-  // },
-  421614: {
-    name: "Arbitrum Testnet",
-    id: 421614,
-    logo: "/logo/arb-logo.png",
-    api: "https://api.spiralstake.xyz",
-    nativeCurrency: {
-      name: "Ether",
-      symbol: "ETH",
-      decimals: 18,
-    },
-    onboard: {
-      amountNative: "0.001",
-    },
+import { defineChain } from "../../node_modules/viem/utils/chain/defineChain.ts";
+
+const fraxtalLocal = defineChain({
+  id: 31337,
+  name: "Frax Local",
+  nativeCurrency: {
+    decimals: 18,
+    name: "Ether",
+    symbol: "ETH",
+  },
+  rpcUrls: {
     default: {
-      http: ["https://arb-sepolia.g.alchemy.com/v2/dP8O0bUE1anNSaJ9Bkub4GoVJoQMIflO"],
-      webSocket: ["wss://arb-sepolia.g.alchemy.com/v2/dP8O0bUE1anNSaJ9Bkub4GoVJoQMIflO"],
+      http: ["http://127.0.0.1:8545"],
+      webSocket: ["ws://127.0.0.1:8545"],
     },
   },
-  // 31337: {
-  //   id: 31337,
-  //   name: "Reya Testnet",
-  //   logo: "/logo/reya-logo.webp",
-  //   api: "http://localhost:5000",
-  //   nativeCurrency: {
-  //     decimals: 18,
-  //     name: "Ether",
-  //     symbol: "ETH",
-  //   },
-  //   onboard: {
-  //     amountNative: "1",
-  //   },
-  //   default: {
-  //     http: ["http://127.0.0.1:8545"],
-  //     webSocket: ["ws://127.0.0.1:8545"],
-  //   },
-  // },
-  // 31338: {
-  //   name: "Frax Testnet",
-  //   id: 31338,
-  //   logo: "/logo/frax-logo.svg",
-  //   api: "http://localhost:5001",
-  //   nativeCurrency: {
-  //     decimals: 18,
-  //     name: "Ether",
-  //     symbol: "ETH",
-  //   },
-  //   onboard: {
-  //     amountNative: "1",
-  //   },
-  //   default: {
-  //     http: ["http://127.0.0.1:8546"],
-  //     webSocket: ["ws://127.0.0.1:8546"],
-  //   },
-  // },
+  logo: "/logo/reya-logo.webp",
+  api: "http://localhost:5000",
+  onboard: {
+    amountNative: "1",
+  },
+});
+
+const arbitrumLocal = defineChain({
+  id: 31338,
+  name: "Arbitrum Local",
+  nativeCurrency: {
+    decimals: 18,
+    name: "Ether",
+    symbol: "ETH",
+  },
+  rpcUrls: {
+    default: {
+      http: ["http://127.0.0.1:8546"],
+      webSocket: ["ws://127.0.0.1:8546"],
+    },
+  },
+  logo: "/logo/arb-logo.png",
+  api: "http://localhost:5001",
+  onboard: {
+    amountNative: "1",
+  },
+});
+
+const fraxtalTestnet = defineChain({
+  id: 2522,
+  name: "Fraxtal Testnet",
+  nativeCurrency: {
+    decimals: 18,
+    name: "Frax Ether",
+    symbol: "frxETH",
+  },
+  rpcUrls: {
+    default: {
+      http: ["https://rpc.testnet.frax.com"],
+    },
+  },
+  logo: "/logo/frax-logo.svg",
+  api: "http://localhost:5000",
+  onboard: {
+    amountNative: "0.00001",
+  },
+});
+
+const arbitrumTestnet = defineChain({
+  id: 421614,
+  name: "Arbitrum Sepolia",
+  nativeCurrency: {
+    decimals: 18,
+    name: "Ether",
+    symbol: "ETH",
+  },
+  rpcUrls: {
+    default: {
+      http: ["https://sepolia-rollup.arbitrum.io/rpc"],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: "Arbiscan",
+      url: "https://sepolia.arbiscan.io",
+      apiUrl: "https://api-sepolia.arbiscan.io/api",
+    },
+  },
+  contracts: {
+    multicall3: {
+      address: "0xca11bde05977b3631167028862be2a173976ca11",
+      blockCreated: 81930,
+    },
+  },
+  testnet: true,
+  logo: "/logo/arb-logo.png",
+  api: "https://api.spiralstake.xyz",
+  onboard: {
+    amountNative: "0.001",
+  },
+});
+
+const reyaTestnet = defineChain({
+  id: 89346162,
+  name: "Reya Cronos",
+  nativeCurrency: {
+    decimals: 18,
+    name: "Ether",
+    symbol: "ETH",
+  },
+  rpcUrls: {
+    default: {
+      http: ["https://rpc.reya-cronos.gelato.digital"],
+      webSocket: ["wss://ws.reya-cronos.gelato.digital"],
+    },
+  },
+  logo: "/logo/reya-logo.webp",
+  api: "https://api.spiralstake.xyz",
+  onboard: {
+    amountNative: "0.0001",
+  },
+});
+
+export const chainConfig = {
+  // 31337: fraxtalLocal,
+  // 31338: arbitrumLocal,
+  2522: fraxtalTestnet,
+  // 421614: arbitrumTestnet,
+  // 89346162: reyaTestnet,
 };
--- src/config/wagmiConfig.js
+++ src/config/wagmiConfig.js
@@ -8,11 +8,12 @@ const chains = Object.values(chainConfig);
 export const wagmiConfig = getDefaultConfig({
   appName: "Spiral Stake",
   projectId: "49bdf41453b575598177350acab135bd",
-  chains: [arbitrumSepolia],
+  chains: [...chains],
   transports: {
-    // [31337]: http("http://127.0.0.1:8545"),
-    // [31338]: http("http://127.0.0.1:8546"),
+    [31337]: http("http://127.0.0.1:8545"),
+    [31338]: http("http://127.0.0.1:8546"),
+    // [2522]: http(chainConfig[2522]?.rpcUrls.default.http[0]),
     // [89346162]: http("https://rpc.reya-cronos.gelato.digital"),
-    [421614]: http("https://sepolia-rollup.arbitrum.io/rpc"),
+    // [421614]: http("https://sepolia-rollup.arbitrum.io/rpc"),
   },
 });
